4c73f32066a460149a0537d87a940b44bd35818a
Sam Lundy <samuel.lundy@gmail.com>
2023-07-26 21:04:52 -0600

    week 3 day 3 homework completed

---
+++ dope_math.py
@@ -0,0 +1,30 @@
+from math import pi
+
+
+def h_area(l, w):
+    '''
+        Function to calculate area of a house, apartment, or other piece of land/property. 
+        ...or anything on a 2d plane really
+        l is length and w is width of rectangle or other shape with 4 sides
+    '''
+    return l * w
+
+
+
+def c_circ(r):
+    '''
+    Function to calculate circumference of a circle
+    r is known radius of circle
+    '''
+    return (2 * pi * r)
+
+
+
+def a_circ(r):
+    '''
+    Bonus function for area of a circle
+    r is known radius of circle
+    '''
+    return (pi * r ** 2)
+
+
